4eac56a33b6ea11f405c6d9f06c4e578b4c990d5
Thaysi Medeiros Freccia <frecciathayse@gmail.com>
2026-09-14 19:19:00 -0300

    Crias as rotas com React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
 import Carrinho from "./pages/Carrinho.jsx";
+import Pagamento from "./pages/Pagamento.jsx";
+import Sucesso from "./pages/Sucesso.jsx";
+import Falha from "./pages/Falha.jsx";
+
 function App() {
-  return <Carrinho />;
+  return (
+    <Routes>
+      <Route path="/" element={<Carrinho />} />
+      <Route path="/pagamento" element={<Pagamento />} />
+      <Route path="/sucesso" element={<Sucesso />} />
+      <Route path="/falha" element={<Falha />} />
+    </Routes>
+  );
 }
 
 export default App;
